60025df3d8fb356d933b3c4b6521828ff215eb44
Filipx21 <filip.zajac2277@gmail.com>
2021-09-01 18:34:11 +0200

    1st button

---
+++ WebForms/WebForms/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebForms.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="tbName" runat="server" Height="42px" OnTextChanged="tbName_TextChanged" Width="286px" AutoPostBack="True"></asp:TextBox>
+            <br />
+            <asp:Button ID="btn1" runat="server" Height="41px" OnClick="Button1_Click" Text="Button" Width="139px" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForms/WebForms/Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForms
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                btn1.Text = "Kliknij mnie";
+            }
+            else
+            {
+                btn1.Text = tbName.Text;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            btn1.Text = tbName.Text;
+        }
+
+        protected void tbName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
