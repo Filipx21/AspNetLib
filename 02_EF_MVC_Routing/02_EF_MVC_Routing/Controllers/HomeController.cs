916e1c45495c21234efac6d4c341f0d3c7e1436b
Filipx21 <filip.zajac2277@gmail.com>
2021-09-02 18:23:11 +0200

    XSS

---
+++ 02_EF_MVC_Routing/02_EF_MVC_Routing/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _02_EF_MVC_Routing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World";
+        }
+
+        //Atak XSS
+        //public string Index()
+        //{
+        //    return "<script>console.log('test')</script>";
+        //}
+
+        //Aby uniknąć XSS
+        //public string Index()
+        //{
+        //    return Server.HtmlEncode("<script>console.log('test')</script>");
+        //}
+    }
+}
