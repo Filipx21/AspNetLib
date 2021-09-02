0ddfb4e1c338f1477487f32cdf422fef1a0e8699
Filipx21 <filip.zajac2277@gmail.com>
2021-09-02 18:30:44 +0200

    Wsteczny routing

--- 02_EF_MVC_Routing/02_EF_MVC_Routing/Controllers/HomeController.cs
+++ 02_EF_MVC_Routing/02_EF_MVC_Routing/Controllers/HomeController.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace _02_EF_MVC_Routing.Controllers
 {
     public class HomeController : Controller
     {
+        //public string Index()
+        //{
+        //    var result = "";
+        //    foreach(var routeKey in RouteData.Values.Keys)
+        //    {
+        //        result += "Key: " + routeKey + ", Value: " + RouteData.Values[routeKey] as string;
+        //        result += "<br/>";
+        //    }
+        //    return result;
+        //}
+
+        //Wsteczny routing
         public string Index()
         {
-            return "Hello World";
+            var path = RouteTable.Routes.GetVirtualPath(null, new RouteValueDictionary(new { Controller = "Kontroler", Action = "Akcja", Id="12", Param="Lipton"}));
+            var result = path.VirtualPath;
+            return result;
         }
 
         //Atak XSS
