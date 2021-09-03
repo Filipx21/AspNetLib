8669e519847c96c808d7ff67b1ab8c01263b6ec3
Filipx21 <filip.zajac2277@gmail.com>
2021-09-03 22:02:37 +0200

    Typy ActionResult

--- 03_EF_MVC_Controller/03_EF_MVC_Controller/Controllers/HomeController.cs
+++ 03_EF_MVC_Controller/03_EF_MVC_Controller/Controllers/HomeController.cs
@@ -6,6 +6,18 @@ using System.Web.Mvc;
 
 namespace _03_EF_MVC_Controller.Controllers
 {
+    /* Dostepne rodzaje ActionResult
+     * - EmptyResult - Brak odpowiedzi; Wykorzyststywane gdy akcja zrwaca null or void
+     * - ContentResult - Odpowiedz jako tekst; Wykorzystywane gdy akcja zwraca inny typ niż ActionResult(string, int)
+     * - JsonResult - Serializuje obiekt do JSON;
+     * - RedirectResult - Przekierowywuje do URL
+     * - RedirectToRouteResult - Przekierowuje na podstawie routingu
+     * - ViewResult - Uruchamia renderowanie przez ViewEngine
+     * - PartialViewResult - Wysyła czesc widoku; Wykorzystywane np. do AJAX
+     * - FileContentResult, FilePathResult, FileStreamResult - Zwraca plik
+     * - JavaScriptResult - Wysyła i wywołuje JS po stronie klienta
+     */
+
     public class HomeController : Controller
     {
         public ActionResult Index()
