f16d888e0aa99b02c629cad919fbf6798ebc230d
Filipx21 <filip.zajac2277@gmail.com>
2021-09-04 19:42:16 +0200

    Razor

---
+++ 07_EF_MVC_View_Razor/07_EF_MVC_View_Razor/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _07_EF_MVC_View_Razor
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
