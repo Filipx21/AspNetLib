84def9ac859a9bf66d1f199c732f2e145d6c11ce
Filipx21 <filip.zajac2277@gmail.com>
2021-09-12 14:19:03 +0200

    myValidator

---
+++ 16_EF_MVC_MyValidator/Validation/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using MyValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddQuestion(Question question)
+        {
+            if (question.QuestionText == "?")
+                ModelState.AddModelError("QuestionText", "Komunikat dodany manualnie. Nie może być samego znaku zapytania...");
+
+            if (!ModelState.IsValid)
+                return View("Index", question);
+            else
+            {
+                // Kod zapisujący lub wysyłający pytanie do właściciela strony
+
+                return View("Index");
+            }
+        }
+
+	}
+}
