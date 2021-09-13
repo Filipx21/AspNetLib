be1335aa217d55e77bc76a72ee5e370b3702d87a
Filipx21 <filip.zajac2277@gmail.com>
2021-09-13 16:28:13 +0200

    QuestionContext

---
+++ 17_EF_MVC_DB/17_EF_MVC_DB/DAL/QuestionContext.cs
@@ -0,0 +1,17 @@
+﻿using _17_EF_MVC_DB.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace _17_EF_MVC_DB.DAL
+{
+    public class QuestionContext : DbContext
+    {
+
+        public QuestionContext() : base("QuestionContext") { }
+
+        public DbSet<Question> Questions { get; set; }
+    }
+}
