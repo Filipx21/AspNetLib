3063251199068f6d768cebdd248abbfe1f245c8d
Filipx21 <filip.zajac2277@gmail.com>
2021-09-14 20:44:48 +0200

    Zmienne statyczne - inkrementacja

--- 20_EF_MVC_ZmienneStatyczne_Application/20_EF_MVC_ZmienneStatyczne_Application/Models/Counters.cs
+++ 20_EF_MVC_ZmienneStatyczne_Application/20_EF_MVC_ZmienneStatyczne_Application/Models/Counters.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace InfrastructureAndState.Models
+﻿namespace _20_EF_MVC_ZmienneStatyczne_Application.Models
 {
     public class Counters
     {
