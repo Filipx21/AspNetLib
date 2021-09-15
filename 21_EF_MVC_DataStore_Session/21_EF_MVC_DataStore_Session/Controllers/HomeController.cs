e230df9a69674c04bd954dc921de65a5d53d7880
Filipx21 <filip.zajac2277@gmail.com>
2021-09-15 20:49:07 +0200

    Session

--- 21_EF_MVC_DataStore_Session/21_EF_MVC_DataStore_Session/Controllers/HomeController.cs
+++ 21_EF_MVC_DataStore_Session/21_EF_MVC_DataStore_Session/Controllers/HomeController.cs
@@ -13,66 +13,41 @@ namespace InfrastructureAndState.Controllers
             return View(counters);
         }
 
-        public ActionResult PRGDemo()
-        {
-            ViewBag.TempParam = TempData["param"];
-
-            return View();
-        }
-
         [HttpPost]
-        public ActionResult PassTempData(string paramtopass)
+        public ActionResult IncrementSession()
         {
-            TempData["param"] = paramtopass;
-
-            return RedirectToAction("PRGDemo");
-        }
-
-        [HttpPost]
-        public ActionResult IncrementStatic()
-        {
-            lock (staticCounterLock)
-            {
-                var counters = GetCounters();
-
-                // Inkrementuj statyczne pole
-                counters.StaticCounter++;
-            }
-
-            return RedirectToAction("Index");
-        }
-
-        [HttpPost]
-        public ActionResult IncrementApplication()
-        {
-            HttpContext.Application.Lock();
             var counters = GetCounters();
-            counters.ApplicationCounter++; // Inkrementuj stan zapisany w Application[]
+
+            // Inkrementuj stan zapisany w Session[]
+            counters.SessionCounter++;
+
             SetCounters(counters);
-            HttpContext.Application.UnLock();
 
             return RedirectToAction("Index");
         }
+
+
 
         private Counters GetCounters()
         {
             var counters = new Counters();
-            if (HttpContext.Application["counter"] != null)
+
+            // Sesja
+            if (Session["counter"] != null)
             {
-                counters.ApplicationCounter = (int)HttpContext.Application["counter"];
+                counters.SessionCounter = (int)Session["counter"];
             }
             else
             {
-                counters.ApplicationCounter = 0;
+                counters.SessionCounter = 0;
             }
-           
+
             return counters;
         }
 
         private void SetCounters(Counters counters)
         {
-            HttpContext.Application["counter"] = counters.ApplicationCounter;
-
+            Session["counter"] = counters.SessionCounter;
         }
 
     }
