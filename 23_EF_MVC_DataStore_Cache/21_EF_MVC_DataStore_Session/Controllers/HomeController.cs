dd87a0a9b12335eac053515b232ee4c50adcf650
Filipx21 <filip.zajac2277@gmail.com>
2021-09-17 20:05:37 +0200

    Cache

---
+++ 23_EF_MVC_DataStore_Cache/21_EF_MVC_DataStore_Session/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Cache_.Models;
+using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cache_.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int id = 1)
+        {
+            var counters = GetCounters();
+            return View(counters);
+        }
+
+        [HttpPost]
+        public ActionResult IncrementCookies()
+        {
+            var counters = GetCounters();
+
+            // Inkrementuj stan zapisany w Cookie
+            counters.CookieCounter++;
+
+            SetCounters(counters);
+
+            return RedirectToAction("Index");
+        }
+
+        private Counters GetCounters()
+        {
+            var counters = new Counters();
+
+            // Cookie
+            if (Request.Cookies["counter"] != null)
+            {
+                counters.CookieCounter = int.Parse(Request.Cookies["counter"].Value);
+            }
+            else
+            {
+                counters.CookieCounter = 0;
+            }
+
+            return counters;
+        }
+
+        private void SetCounters(Counters counters)
+        {
+            HttpCookie cookie = new HttpCookie("counter", counters.CookieCounter.ToString());
+            //cookie.Expires = DateTime.Now.AddDays(-1);
+            cookie.Expires = DateTime.Now.AddDays(1);
+            Response.SetCookie(cookie);
+        }
+
+    }
+}
