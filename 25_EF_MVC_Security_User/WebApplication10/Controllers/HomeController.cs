1a62f9697cdb906fbfdca92d496079e8f8a70226
Filipx21 <filip.zajac2277@gmail.com>
2021-09-19 15:18:45 +0200

    User_Auth

---
+++ 25_EF_MVC_Security_User/WebApplication10/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication10.Models;
+
+namespace WebApplication10.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            // LOGIN: Kazio lub Bartek
+            // Hasło: P@ssw0rd
+
+            var currentUserId = User.Identity.GetUserId();
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var currentUser = manager.FindById(currentUserId);
+
+            var r = currentUser.Roles;
+            ViewBag.ProfileInformation = currentUser.UserInfo.FirstName;
+
+            return View();
+        }
+
+
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ 25_EF_MVC_Security_User/WebApplication10/Models/IdentityModels.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace WebApplication10.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        
+        public virtual MyUserInfo UserInfo { get; set; }
+
+
+    }
+
+
+    public class MyUserInfo
+    {
+        public int MyUserInfoId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    
+
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+}
