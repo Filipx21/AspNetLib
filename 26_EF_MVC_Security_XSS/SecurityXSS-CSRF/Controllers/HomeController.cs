2d12ca7aa21c58b9c636493de49faeb6283c51ad
Filipx21 <filip.zajac2277@gmail.com>
2021-09-19 15:51:29 +0200

    XSS

---
+++ 26_EF_MVC_Security_XSS/SecurityXSS-CSRF/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Security.Application;
+using SecurityXSS_CSRF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecurityXSS_CSRF.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<Question> Questions { get; set; }
+
+        static HomeController()
+        {
+            HomeController.Questions = new List<Question>();
+        }
+
+        public ActionResult Index()
+        {
+            return View(HomeController.Questions);
+        }
+
+        [ValidateInput(false)]
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult AddQuestion(Question newQuestion)
+        {
+            //newQuestion.QuestionText = Sanitizer.GetSafeHtmlFragment(newQuestion.QuestionText);
+            
+            HomeController.Questions.Add(newQuestion);
+            return RedirectToAction("Index");
+        }
+
+	}
+}
---
+++ 26_EF_MVC_Security_XSS/SecurityXSS-CSRF/README.md
@@ -0,0 +1,17 @@
+﻿### Cross Site Scripting (XSS):
+[Polega na osadzeniu w treści atakowanej strony kodu dodatkowego kodu JS, 
+który może doprowadzić do niepożądanych akcji.] 
+* Wstrzykiwanie kodu JS do aplikacji internetowej:
+	* Kradzież ciasyteczek (w tym dane sesji),
+	* Modyfikacja zawartosci strony internetowej,
+* Active injection: Polega na tym, ze strona powtarza wprowadzone przez usera parametry. Takie jak elementy form, querry string.
+* Passive injection: Sposób wstrzykniecia informacji do bazy danych.
+
+### Zabezpieczenie:
+* Nigdy nie ufaj danym od użytkownika,
+* Stosować domyślnych zabezpieczenień przed przeslaniem niebezpiecznej tresci
+* Czasami konieczne jest wylącznie (np.CMS):
+	* [ValidateInput(false)],
+	* Http.Raw - na stronie,
+	* Biblioteka AntiXSS (bardzo restrykcyjna), do wyfiltrowania bezpiecznej zawartosci.
+	
